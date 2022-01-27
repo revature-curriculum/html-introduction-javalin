9beb012f5d277200d6f02441d331d053d296efd2
nballar <naballar@gmail.com>
2022-01-27 08:57:12 -0800

    added required files

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Javalin app = Javalin.create(config -> {config.addStaticFiles("/public", Location.CLASSPATH);}).start(4100);
+        
+        // app.get("/", ctx -> ctx.render("index.html"));
+
+    }
+}
